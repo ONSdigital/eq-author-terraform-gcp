eef39fdd8e840a8c9aa77c91a5f9f774785a28bd
martyncolmer <martyc1974@hotmail.com>
2021-08-06 19:20:16 +0100

    added firestore permissions

--- applications/iam.tf
+++ applications/iam.tf
@@ -5,11 +5,15 @@ resource "google_service_account" "cloud_run_service_account" {
 }
 resource "google_project_iam_member" "editor" {
   role   = "roles/run.admin"
-
   member = "serviceAccount:${google_service_account.cloud_run_service_account.email}"
 }
 
 resource "google_project_iam_member" "secret_accessor" {
   role   = "roles/secretmanager.secretAccessor"
+  member = "serviceAccount:${google_service_account.cloud_run_service_account.email}"
+}
+
+resource "google_project_iam_member" "datastore_user" {
+  role   = "roles/datastore.user"
   member = "serviceAccount:${google_service_account.cloud_run_service_account.email}"
 }
