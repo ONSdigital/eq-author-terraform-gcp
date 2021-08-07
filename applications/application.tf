4f2eb011492fcdf629c153f7370cecae61096e7c
martyncolmer <martyc1974@hotmail.com>
2021-08-07 22:37:20 +0100

    tweak

--- applications/application.tf
+++ applications/application.tf
@@ -65,7 +65,7 @@ resource "google_cloud_run_service" "default" {
     
     lifecycle {
         ignore_changes = [
-        template[0].spec[0].containers[0].image
+        template[0].spec[0].containers[0].image,
         metadata[0].annotations["run.googleapis.com/launch-stage"]
         ]
     }
