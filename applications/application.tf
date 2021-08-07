4217ecb309a7ec3ba6bfea58df98238cab156f87
martyncolmer <martyc1974@hotmail.com>
2021-08-07 23:06:45 +0100

    tweak

--- applications/application.tf
+++ applications/application.tf
@@ -68,6 +68,7 @@ resource "google_cloud_run_service" "default" {
         template[0].spec[0].containers[0].image,
         template[0].metadata[0].annotations,
         metadata[0].annotations
+        status
         ]
     }
 }
