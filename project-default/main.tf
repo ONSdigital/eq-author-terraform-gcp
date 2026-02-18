a82a0c4843fdfd143a691a0e8ea641cfb6c8bb0e
Martyn <46242834+martyncolmer@users.noreply.github.com>
2026-02-18 20:37:06 +0000

    bump
    Signed-off-by: Martyn <46242834+martyncolmer@users.noreply.github.com>

--- project-default/main.tf
+++ project-default/main.tf
@@ -1,11 +1,5 @@
 terraform {
   backend "gcs" {}
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> 5.0.0"
-    }
-  }
   required_version = "~> 1.5.7"
 }
 
