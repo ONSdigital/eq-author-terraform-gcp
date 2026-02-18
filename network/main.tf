4d639da70376cbc05c31caa5859c6ea932e44949
Martyn <46242834+martyncolmer@users.noreply.github.com>
2026-02-18 20:18:23 +0000

    bump
    Signed-off-by: Martyn <46242834+martyncolmer@users.noreply.github.com>

--- network/main.tf
+++ network/main.tf
@@ -1,7 +1,7 @@
 
 module "project-services" {
   source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  version = "~> 18.2"
+  version = "~> 10.0"
 
   project_id = var.project_id
 
